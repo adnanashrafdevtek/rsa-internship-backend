1806c0389e3bce5f41a004228d9c8ae2e38409d0
Muhammad Hussein <muhammadrides@gmail.com>
2025-08-02 10:29:01 -0500

    Update db.js

--- db.js
+++ db.js
@@ -4,11 +4,20 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+pool.getConnection()
+  .then(conn => {
+    console.log('Connected to MySQL!');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('MySQL connection error:', err.message);
+  });
 
 module.exports = pool;
